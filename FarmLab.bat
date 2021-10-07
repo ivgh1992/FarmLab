411a9aff4eed126adfe0ea2509aba90a4aecbe39
ivgh1992 <65133844+ivgh1992@users.noreply.github.com>
2021-10-07 21:48:22 +0200

    Add files via upload

---
+++ FarmLab.bat
@@ -0,0 +1,6 @@
+::Write the path to the activate.bat file of anaconda in your PC
+call C:\Users\MyUser\anaconda3\Scripts\activate.bat
+::Write the path to the FarmLab_Dashboard file in your PC
+cd  C:\Users\MyUser\Desktop\FarmLab\FarmLab_Dashboard
+::Launch the python application
+python FarmLab.py
